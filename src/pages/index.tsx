b6e4ce0dfdba3b6e265cd4688678612b5ba66303
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2022-12-15 22:57:19 -0300

    feat: Connection Strapi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,10 @@ import Footer from 'components/Footer'
 import SectionOne from 'components/SectionOne'
 import SectionTwo from 'components/SectionTwo'
 
+import { GetStaticProps } from 'next'
+import client from 'graphql/client'
+import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
+
 export default function Home() {
   return (
     <>
@@ -25,4 +29,14 @@ export default function Home() {
       />
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { landingPage } = await client.request(GET_LANDING_PAGE)
+  console.log(landingPage.data.attributes)
+  return {
+    props: {
+      ...landingPage.data.attributes
+    }
+  }
 }
